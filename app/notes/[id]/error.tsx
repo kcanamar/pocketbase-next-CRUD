65187cb4de2f936eff7dbfaae4e9e5073c6609b9
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-05-19 10:32:07 -0700

    wip: loading && error components

---
+++ app/notes/[id]/error.tsx
@@ -0,0 +1,4 @@
+// Component to be displayed when an error occurs
+export default function Error() {
+  return <p>error...</p>
+}
---
+++ app/notes/[id]/loading.tsx
@@ -0,0 +1,4 @@
+// This component will be displayed while the content for the dynamic route is being fetched, providing faster load times
+export default function Loading() {
+  return <p>Loading...</p>
+}
